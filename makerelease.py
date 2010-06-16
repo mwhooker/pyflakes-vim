ff48cdfab7613253894a3c4f9fb653f3cdae09de
Matthew Hooker <mhooker@digg.com>
2010-06-16 17:54:56 -0400

    fix for python 2.5

--- makerelease.py
+++ makerelease.py
@@ -3,6 +3,9 @@ creates the pyflakes-vim.zip bundle
 '''
 
 import os.path
+import posixpath
+from posixpath import curdir, sep, pardir, join
+
 import zipfile
 
 BUNDLE_FILENAME = 'pyflakes-vim.zip'
@@ -29,7 +32,7 @@ def make_dist():
         for f in files:
             name = os.path.join(root, f)
             if include_file(f):
-                zipname = os.path.relpath(name, base)
+                zipname = relpath(name, base)
                 print zipname
                 count += 1
                 z.writestr(zipname, open(name, 'rb').read())
@@ -37,6 +40,19 @@ def make_dist():
 
     print
     print '%s is %d files, %d bytes' % (BUNDLE_FILENAME, count, os.path.getsize(BUNDLE_FILENAME))
+
+def relpath(path, start=curdir):
+    """Return a relative version of a path"""
+    if not path:
+        raise ValueError("no path specified")
+    start_list = posixpath.abspath(start).split(sep)
+    path_list = posixpath.abspath(path).split(sep)
+    # Work out how much of the filepath is shared by start and path.
+    i = len(posixpath.commonprefix([start_list, path_list]))
+    rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
+    if not rel_list:
+        return curdir
+    return join(*rel_list)
 
 if __name__ == '__main__':
     make_dist()
